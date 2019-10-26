3af2736c9e2fc1619b6f0a7fbb97e43c51cecac6
Cyril Urban <cyril.urban@qest.cz>
2019-10-26 15:39:17 +0200

    feat(search-components): Start with search components - search box, switch, slider, datepicker

--- web-app/src/features/Main/index.tsx
+++ web-app/src/features/Main/index.tsx
@@ -1,24 +1,68 @@
-import { Layout } from 'antd';
 import React from 'react'
+import { Formik } from 'formik'
 
-import { StyledLayout } from './styled';
+import { StyledBody, StyledHeader, StyledLogo, StyledInput, MainLayout } from './styled';
+// import { Flex } from 'components/Layout/styled';
+import { Button, Icon, DatePicker, Slider, InputNumber, Switch } from 'antd';
+import moment from 'moment';
+import { Flex } from 'components/Layout/styled';
+moment.locale('cs');
 
-const { Header} = Layout;
-
-const Main = ({ location: { search }, match: { params } }) => {
+const Main = ({ }) => {
   // const credentials = qs.parse(search, { ignoreQueryPrefix: true })
   console.log('main')
   return (
-    <Layout>
-      <Header style={{ height: '40px', lineHeight: '40px' }}>
-        <div style={{ color: 'white' }} > Flicker++</div>
-      </Header>
-        <StyledLayout>
-          <h1>Flicker app</h1>
-          <p>heloooooo</p>
+    <MainLayout>
+      <StyledHeader>
+        <StyledLogo> Flickr++</StyledLogo>
+      </StyledHeader>
+      <StyledBody>
+        <Formik
+          initialValues={{ search: '', uploaded: moment(new Date, 'DD/MM/YYYY'), uploadedWeight: 50, uploadedChecked: false }}
+          // validationSchema={ValidationSchoolEditMode}
+          // validateOnChange={true}
+          onSubmit={data => {
+            console.log(data)
+          }}
+        >
+          {({ submitForm, values, handleChange, setFieldValue }) => (
+            <>
+              {/* <Wrapper> */}
+              <br></br>
+              <StyledInput value={values.search} prefix={<Icon type="search"></Icon>} placeholder="Photos, people or groups" onChange={handleChange}></StyledInput>
+              <br></br>
 
-        </StyledLayout>
-    </Layout>
+              <Flex direction="row">
+                <Switch defaultChecked={values.uploadedChecked} onChange={(data) => setFieldValue('uploadedChecked', data)} />
+                <DatePicker value={values.uploaded} format={'DD/MM/YYYY'} onChange={(data) => setFieldValue('uploaded', moment(data, 'DD/MM/YYYY'))}/>
+                <div style={{ width: '150px' }} >
+                  <Slider
+                    min={1}
+                    max={100}
+                    onChange={(data) => setFieldValue('uploadedWeight', data)}
+                    value={values.uploadedWeight}
+                  />
+                </div>
+                <InputNumber
+                  min={1}
+                  max={100}
+                  style={{ marginLeft: 16 }}
+                  value={values.uploadedWeight}
+                  onChange={(data) => setFieldValue('uploadedWeight', data)}
+                />
+              </Flex>
+
+              <br></br>
+
+              <Button onClick={() => submitForm()}>Search</Button>
+              {/* </Wrapper> */}
+            </>
+          )}
+        </Formik>
+
+
+      </StyledBody>
+    </MainLayout>
   )
 }
 export default Main
--- web-app/src/features/Main/styled.ts
+++ web-app/src/features/Main/styled.ts
@@ -1,55 +1,38 @@
-import { Button, Divider, Layout } from 'antd'
-import { Flex } from 'components/Layout/styled'
+import { Input, Layout } from 'antd'
 import styled from 'styled-components'
 
-export const StyledLayout = styled(Layout)`
-  height: 100vh !important;
+export const StyledHeader = styled(Layout)`
+  height: 50px;
+  line-height: 50px;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  background: black !important;
+`
+
+export const MainLayout = styled(Layout)`
+  width: 100%;
   display: flex;
   align-items: center;
   justify-content: center;
-  background: #f0f2f5;
+  /* background: #f0f2f5; */
 `
 
-export const Wrapper = styled.div`
-  height: 85vh;
-  width: 90vw;
-  max-height: 900px;
-  max-width: 1400px;
-  overflow: hidden;
-  border-radius: 10px;
-  background: red !important;
+export const StyledBody = styled(Layout)`
+  height: 100vh;
+  width: 80%;
+  display: flex;
+  align-items: center;
+  background: white !important;
+`
+export const StyledLogo = styled.div`
+  color: white;
+  width: 80%;
+  font-size: 26px;
+  font-weight: bold;
 `
 
-export const StyledTabWrapper = styled(Flex)`
-  border-bottom: ${({ theme }) => `1px solid ${theme.colors.lightGrey}`};
-  padding-top: 8px;
-`
-
-export const StyledLink = styled(Button)`
-  text-transform: uppercase;
-  font-size: 16px;
-  padding: 0px !important;
-`
-
-export const StyledContent = styled.div`
-  overflow-y: scroll;
-  height: 90%;
-  padding: 0 40 15 40;
-`
-
-export const DefaultTabStyle = {
-  height: '100%',
-  padding: '0 40',
-  margin: '0',
-  display: 'flex',
-  textTransform: 'uppercase',
-  border: 'none',
-}
-
-export const StyledDivider = styled(Divider)`
-  background: ${({ theme }) => theme.colors.primary};
-`
-
-export const StyledFlex = styled(Flex)`
-  padding: 0px 22px 0px 0px;
+export const StyledInput = styled(Input)`
+  max-width: 500px;
+  height: 30px;
 `
